1ea9586e1cbded162a2b2b38923ab4fa411e0250
jasdeepUNSW <33558491+jasdeepUNSW@users.noreply.github.com>
2019-04-27 21:58:32 +1000

    Update Dijkstra.c

--- Dijkstra.c
+++ Dijkstra.c
@@ -6,6 +6,7 @@
 #include <stdio.h>
 #include <limits.h>
 
+static bool inPredNodeList(PredNode* L, Vertex ver);
 
 //shortest distance complete
 //working on pred path - almost there
@@ -35,7 +36,7 @@ ShortestPaths dijkstra(Graph g, Vertex v) {
 	PQ priorQ = newPQ();
 
 	//add adjacent vertices and edge weights 
-    	AdjList l = outIncident(g, v);
+    AdjList l = outIncident(g, v);
 	AdjList curr = l;
 	while (curr != NULL){
 		ItemPQ new;
@@ -63,9 +64,34 @@ ShortestPaths dijkstra(Graph g, Vertex v) {
 				uVertex.key = curr->w;
 				uVertex.value = curr->weight;
 				addPQ(priorQ, uVertex);
+				if(throwAway.pred[curr->w] == NULL){
+				    PredNode *new = malloc(sizeof(PredNode));
+				    throwAway.pred[curr->w] = new;
+				    new->v = u;
+				    new->next = NULL;
+				} else {
+			        PredNode *new = malloc(sizeof(PredNode));
+			        throwAway.pred[curr->w] = new;
+			        new->v = u;
+			        new->next = NULL;
+				}
+			}
+			if (throwAway.dist[curr->w] == (throwAway.dist[u] + curr->weight)){
+			    if (!inPredNodeList(throwAway.pred[curr->w], u)){
+			        PredNode *new = malloc(sizeof(PredNode));
+			        new->v = u;
+		            new->next = throwAway.pred[curr->w];
+		            throwAway.pred[curr->w] = new;
+			    }
 			}
 			curr = curr->next;
 		}
+	}
+	
+	for(int i = 0; i < throwAway.noNodes; i++){
+	    if(throwAway.dist[i] == INT_MAX){
+	        throwAway.dist[i] = 0;
+	    }
 	}
 	freePQ(priorQ);
 	return throwAway;
@@ -91,3 +117,15 @@ void  freeShortestPaths(ShortestPaths paths) {
     free(paths.dist);
     //free the linked list pred
 }
+
+static bool inPredNodeList(PredNode* L, Vertex ver){
+    PredNode *curr = L;
+    while(curr != NULL){
+        if (curr->v == ver){
+            return true;
+        }
+        curr = curr->next;
+    }
+    return false;
+}
+
